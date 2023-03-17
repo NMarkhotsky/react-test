83fd8900a93163cd6abae6125d9809d2df5bef2d
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-03-17 19:11:26 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,9 @@
-import { RecipeList } from './RecipeList/RecipeList';
-import recipes from 'recipes';
 import { GlobalStyle } from 'components/GlobalStyle';
 import { Layout } from './Layout/Layout';
 
 export const App = () => {
   return (
     <Layout>
-      <RecipeList items={recipes} />
       <GlobalStyle />
     </Layout>
   );
